c00fe41f2657e9b7da6b9e252ab54b86f383a9bf
Azat <kaumov.a.r@gmail.com>
2022-06-21 21:08:30 +0200

    Add Song model

---
+++ LyraPlay/Domain/Models/Song.swift
@@ -0,0 +1,15 @@
+//
+//  Song.swift
+//  LyraPlay
+//
+//  Created by Azat Kaiumov on 21.06.22.
+//
+
+import Foundation
+
+struct Song {
+    var id: UUID
+    var name: String
+    var artist: String
+    var durationSeconds: Int
+}
