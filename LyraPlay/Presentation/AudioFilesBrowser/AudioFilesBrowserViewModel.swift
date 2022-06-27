33f68ea0c0204807a873cd081f9fe73cbfd9ad07
Azat <azat4dev@gmail.com>
2022-06-27 15:02:09 +0200

    Add implementation for AudioFilesBrowserViewModel

--- LyraPlay/Presentation/AudioFilesBrowser/AudioFilesBrowserViewModel.swift
+++ LyraPlay/Presentation/AudioFilesBrowser/AudioFilesBrowserViewModel.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+
+// MARK: - Interfaces
+
+public struct FileBrowserItem {
+    
+    public var id: UUID
+    public var title: String
+    public var description: String
+}
+
+public protocol AudioFilesBrowserViewModelOutput {
+    
+    var isLoading: Observable<Bool> { get }
+    var files: Observable<[FileBrowserItem]> { get }
+}
+
+public protocol AudioFilesBrowserViewModelInput {
+    
+    func load() async
+}
+
+public protocol AudioFilesBrowserViewModel: AudioFilesBrowserViewModelInput, AudioFilesBrowserViewModelOutput {
+}
+
+// MARK: - Implementations
+
+public final class DefaultAudioFilesBrowserViewModel: AudioFilesBrowserViewModel {
+
+    private let browseUseCase: BrowseAudioFilesUseCase
+    public var files: Observable<[FileBrowserItem]>
+    public var isLoading: Observable<Bool>
+    
+    public init(browseUseCase: BrowseAudioFilesUseCase) {
+        
+        self.browseUseCase = browseUseCase
+        self.isLoading = Observable(true)
+        self.files = Observable([])
+    }
+    
+    public func load() async {
+        
+        isLoading.value = true
+        
+        let result = await browseUseCase.listFiles()
+        
+        guard case .success(let loadedFiles) = result else {
+            return
+        }
+        
+        self.files.value = loadedFiles.map { loadedFile in
+            return FileBrowserItem(
+                id: loadedFile.id!,
+                title: loadedFile.name,
+                description: loadedFile.artist ?? ""
+            )
+        }
+        
+        self.isLoading.value = false
+    }
+}
--- LyraPlayTests/Presentation/AudioFilesBrowser/AudioFilesBrowserViewModelTests.swift
+++ LyraPlayTests/Presentation/AudioFilesBrowser/AudioFilesBrowserViewModelTests.swift
@@ -11,47 +11,53 @@ import XCTest
 
 class AudioFilesBrowserViewModelTests: XCTestCase {
 
-//    private var filesRepository: AudioFilesRepository!
-//    private var useCase: BrowseAudioFilesUseCase!
-//    private var viewModel: AudioFilesBrowserViewModel!
-//    
-//    override func setUp() async throws {
-//        
-//        filesRepository = AudioFilesRepositoryMock()
-//        useCase = DefaultBrowseAudioFilesUseCase(audioFilesRepository: filesRepository)
-//        viewModel = DefaultAudioFilesBrowserViewModel(browseUseCase: useCase)
-//    }
-//    
-//    private func getTestFile(index: Int) -> (info: AudioFileInfo, data: Data) {
-//        return (
-//            info: AudioFileInfo.create(name: "Test \(index)"),
-//            data: "Test \(index)".data(using: .utf8)!
-//        )
-//    }
-//    
-//    func testListFiles() async throws {
-//        
-//        let numberOfTestFiles = 5
-//        let testFiles = (0..<numberOfTestFiles).map { self.getTestFile(index: $0) }
-//        
-//        for file in testFiles {
-//            let _ = await filesRepository.putFile(info: file.info, data: file.data)
-//        }
-//        
-//        let expectation = XCTestExpectation()
-//        
-//        viewModel.bind(self) { [weak self] files in
-//     
-//            let expectedTitles = testFiles.map { $0.name }
-//            let expectedDescriptions  = testFiles.map { $0.description }
-//            
-//            XCTAssertEqual(files.map { $0.title }, expectedTitles)
-//            XCTAssertEqual(files.map { $0.description }, expectedDescriptions)
-//            
-//            expectation.fulfill()
-//        }
-//        
-//        await waitForExpectations(timeout: 5, handler: nil)
-//    }
+    private var filesRepository: AudioFilesRepository!
+    private var useCase: BrowseAudioFilesUseCase!
+    private var viewModel: AudioFilesBrowserViewModel!
+    
+    override func setUp() async throws {
+        
+        filesRepository = AudioFilesRepositoryMock()
+        useCase = DefaultBrowseAudioFilesUseCase(audioFilesRepository: filesRepository)
+        viewModel = DefaultAudioFilesBrowserViewModel(browseUseCase: useCase)
+    }
+    
+    private func getTestFile(index: Int) -> (info: AudioFileInfo, data: Data) {
+        return (
+            info: AudioFileInfo.create(name: "Test \(index)"),
+            data: "Test \(index)".data(using: .utf8)!
+        )
+    }
+    
+    func testListFiles() async throws {
+        
+        let numberOfTestFiles = 5
+        let testFiles = (0..<numberOfTestFiles).map { self.getTestFile(index: $0) }
+        
+        for file in testFiles {
+            let _ = await filesRepository.putFile(info: file.info, data: file.data)
+        }
+        
+        let expectation = XCTestExpectation()
+        
+        viewModel.files.observe(on: self) { [weak self] files in
+            
+            if self?.viewModel.isLoading.value ?? false {
+                return
+            }
+     
+            let expectedTitles = testFiles.map { $0.info.name }
+            let expectedDescriptions  = testFiles.map { $0.info.artist ?? "" }
+            
+            XCTAssertEqual(files.map { $0.title }, expectedTitles)
+            XCTAssertEqual(files.map { $0.description }, expectedDescriptions)
+            
+            expectation.fulfill()
+        }
+        
+
+        await viewModel.load()
+        wait(for: [expectation], timeout: 3, enforceOrder: true)
+    }
 }
 
