839c48dd09820dd8f57a23cb15884be9891bf6e8
Azat <azat4dev@gmail.com>
2022-06-28 04:56:29 +0200

    Add FilesRepository protocol

---
+++ LyraPlay/Domain/Interfaces/Repositories/FilesRepository.swift
@@ -0,0 +1,25 @@
+//
+//  FilesRepository.swift
+//  LyraPlay
+//
+//  Created by Azat Kaiumov on 28.06.22.
+//
+
+import Foundation
+
+// MARK: - Interfaces
+
+public enum FilesRepositoryError: Error {
+    
+    case internalError
+    case fileNotFound
+}
+
+public protocol FilesRepository {
+    
+    func putFile(name: String, data: Data) async -> Result<URL, FilesRepositoryError>
+    
+    func getFile(name: String) async -> Result<Data, FilesRepositoryError>
+    
+    func deleteFile(name: String) async -> Result<Data, FilesRepositoryError>
+}
