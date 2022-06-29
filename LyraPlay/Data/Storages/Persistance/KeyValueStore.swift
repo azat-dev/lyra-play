422964ee249fc4da37f44f4c2838c12d83605a86
Azat <azat4dev@gmail.com>
2022-06-29 18:34:23 +0200

    Add implementation for the UserDefaultsKeyValueStoreTests

--- LyraPlay/Data/Storages/Persistance/KeyValueStore.swift
+++ LyraPlay/Data/Storages/Persistance/KeyValueStore.swift
@@ -7,13 +7,140 @@
 
 import Foundation
 
+// MARK: - Interfaces
+
 public protocol KeyValueStore {
     
     func get<T: Codable>(key: String, as: T.Type) async -> Result<T?, Error>
     
+    @discardableResult
     func put<T: Codable>(key: String, value: T) async -> Result<Void, Error>
     
+    @discardableResult
     func delete(key: String) async -> Result<Void, Error>
     
     func listKeys() async -> Result<[String], Error>
+    
+    @discardableResult
+    func deleteAll() async -> Result<Void, Error>
+}
+
+// MARK: - Implementations
+
+public final class UserDefaultsKeyValueStore: KeyValueStore {
+    
+    private let storeName: String
+    private let userDefaults: UserDefaults
+    
+    public init(storeName: String) {
+        
+        self.storeName = storeName
+        self.userDefaults = UserDefaults.standard
+    }
+    
+    private func getKeyWithPrefix(key: String) -> String {
+        
+        let encodedPrefix = storeName.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+        let encodedKey = key.addingPercentEncoding(withAllowedCharacters: .alphanumerics)!
+        
+        return "\(encodedPrefix)/\(encodedKey)"
+    }
+    
+    private func getKeyWithoutPrefix(key: String) -> String? {
+        
+        let splitted = key.components(separatedBy: "/")
+        
+        guard splitted.count == 2 else {
+            return nil
+        }
+        
+        let encodedPrefix = splitted[0]
+        
+        let decodedPrefix = encodedPrefix.removingPercentEncoding
+        
+        guard decodedPrefix == storeName else {
+            return nil
+        }
+        
+        let encodedKey = splitted[1]
+        
+        return encodedKey.removingPercentEncoding
+    }
+    
+    public func get<T: Codable>(key: String, as type: T.Type) async -> Result<T?, Error> {
+        
+        let keyWithPrefix = getKeyWithPrefix(key: key)
+        let encodedValue = userDefaults.data(forKey: keyWithPrefix)
+        
+        guard let encodedValue = encodedValue else {
+            return .success(nil)
+        }
+        
+        let decoder = JSONDecoder()
+        
+        do {
+
+            let decodedValue = try decoder.decode(type, from: encodedValue)
+            return .success(decodedValue)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    @discardableResult
+    public func put<T: Codable>(key: String, value: T) async -> Result<Void, Error> {
+        
+        let keyWithPrefix = getKeyWithPrefix(key: key)
+        let encoder = JSONEncoder()
+
+        do {
+            
+            let encodedValue = try encoder.encode(value)
+            userDefaults.set(encodedValue, forKey: keyWithPrefix)
+            return .success(())
+            
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    @discardableResult
+    public func delete(key: String) async -> Result<Void, Error> {
+        
+        let keyWithPrefix = getKeyWithPrefix(key: key)
+        userDefaults.removeObject(forKey: keyWithPrefix)
+        
+        return .success(())
+    }
+    
+    public func listKeys() async -> Result<[String], Error> {
+        
+        let dictionary = userDefaults.dictionaryRepresentation()
+
+        let keys = dictionary.keys.compactMap { getKeyWithoutPrefix(key: $0) }
+        return .success(keys)
+    }
+    
+    @discardableResult
+    public func deleteAll() async -> Result<Void, Error> {
+        
+        let resultKeys = await listKeys()
+        
+        switch resultKeys {
+        
+        case .success(let keys):
+            for key in keys {
+                
+                let result = await delete(key: key)
+                if case .failure(let error) = result {
+                    return .failure(error)
+                }
+            }
+            
+            return .success(())
+            
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
 }
--- LyraPlayTests/Data/Stores/UserDefaultsKeyValueStoreTests.swift
+++ LyraPlayTests/Data/Stores/UserDefaultsKeyValueStoreTests.swift
@@ -13,48 +13,60 @@ import XCTest
 class UserDefaultsKeyValueStoreTests: XCTestCase {
 
     private var keyValueStore: KeyValueStore!
+
+    private func deleteAllKeys() async {
+        let store1 = UserDefaultsKeyValueStore(storeName: "test1")
+        let store2 = UserDefaultsKeyValueStore(storeName: "test2")
+        
+        await store1.deleteAll()
+        await store2.deleteAll()
+    }
     
     override func setUp() async throws {
-
-        keyValueStore = UserDefaultsKeyValueStore(prefix: "test")
+        await deleteAllKeys()
     }
-
+    
     override func tearDown() async throws {
-        
-        let keysResult = await keyValueStore.listKeys()
-        let keys = AssertResultSucceded(keysResult)
-        
-        for key in keys {
-            await keyValueStore.delete(key: key)
-        }
+        await deleteAllKeys()
     }
     
     func testPutGet() async {
         
+        let keyValueStore1 = UserDefaultsKeyValueStore(storeName: "test1")
+        let keyValueStore2 = UserDefaultsKeyValueStore(storeName: "test2")
+        
         let testStruct = TestStruct(a: "test", b: 1)
         let testNumber = 1
+        let testNumber2 = 2
         let testText = "123"
         
-        await keyValueStore.put(key: "1", value: testStruct)
-        await keyValueStore.put(key: "2", value: testNumber)
-        await keyValueStore.put(key: "3", value: testText)
+        await keyValueStore1.put(key: "1", value: testStruct)
+        await keyValueStore2.put(key: "1", value: testNumber2)
+        await keyValueStore1.put(key: "2", value: testNumber)
+        await keyValueStore1.put(key: "3", value: testText)
         
-        let resultReceivedValue1 = await keyValueStore.get(key: "1", as: TestStruct.self)
+        let resultReceivedValue1 = await keyValueStore1.get(key: "1", as: TestStruct.self)
         let receivedValue1 = AssertResultSucceded(resultReceivedValue1)
         XCTAssertNotNil(receivedValue1)
         XCTAssertEqual(receivedValue1, testStruct)
         
-        let resultReceivedValue2 = await keyValueStore.get(key: "2", as: Int.self)
+        let resultReceivedValue2 = await keyValueStore1.get(key: "2", as: Int.self)
         let receivedValue2 = AssertResultSucceded(resultReceivedValue2)
         XCTAssertEqual(receivedValue2, testNumber)
         
-        let resultReceivedValue3 = await keyValueStore.get(key: "3", as: String.self)
+        let resultReceivedValue3 = await keyValueStore1.get(key: "3", as: String.self)
         let receivedValue3 = AssertResultSucceded(resultReceivedValue3)
         XCTAssertEqual(receivedValue3, testText)
+        
+        let resultReceivedValue22 = await keyValueStore2.get(key: "1", as: Int.self)
+        let receivedValue22 = AssertResultSucceded(resultReceivedValue22)
+        XCTAssertEqual(receivedValue22, testNumber2)
     }
     
     func testGetNotExisitingKey() async {
      
+        let keyValueStore = UserDefaultsKeyValueStore(storeName: "test1")
+        
         let resultReceivedValue = await keyValueStore.get(key: "1", as: Int.self)
         let receivedValue = AssertResultSucceded(resultReceivedValue)
         XCTAssertNil(receivedValue)
@@ -62,59 +74,116 @@ class UserDefaultsKeyValueStoreTests: XCTestCase {
     
     func testUpdateKey() async {
         
+        let keyValueStore1 = UserDefaultsKeyValueStore(storeName: "test1")
+        let keyValueStore2 = UserDefaultsKeyValueStore(storeName: "test2")
+        
         let testNumber1 = 1
         let testNumber1Updated = 3
         
         let testNumber2 = 2
         
-        await keyValueStore.put(key: "1", value: testNumber1)
-        await keyValueStore.put(key: "2", value: testNumber2)
-        await keyValueStore.put(key: "1", value: testNumber1Updated)
+        let testNumber22 = 5
+        
+        await keyValueStore2.put(key: "1", value: testNumber22)
+        await keyValueStore1.put(key: "1", value: testNumber1)
+        await keyValueStore1.put(key: "2", value: testNumber2)
+        await keyValueStore1.put(key: "1", value: testNumber1Updated)
+        
 
-        let resultReceivedValue1 = await keyValueStore.get(key: "1", as: Int.self)
+        let resultReceivedValue1 = await keyValueStore1.get(key: "1", as: Int.self)
         let receivedValue1 = AssertResultSucceded(resultReceivedValue1)
         XCTAssertEqual(receivedValue1, testNumber1Updated)
         
-        let resultReceivedValue2 = await keyValueStore.get(key: "2", as: Int.self)
+        let resultReceivedValue2 = await keyValueStore1.get(key: "2", as: Int.self)
         let receivedValue2 = AssertResultSucceded(resultReceivedValue2)
         XCTAssertEqual(receivedValue2, testNumber2)
+        
+        let resultReceivedValue22 = await keyValueStore2.get(key: "1", as: Int.self)
+        let receivedValue22 = AssertResultSucceded(resultReceivedValue22)
+        XCTAssertEqual(receivedValue22, testNumber22)
     }
     
     func testListKeys() async {
         
+        let keyValueStore1 = UserDefaultsKeyValueStore(storeName: "test1")
+        let keyValueStore2 = UserDefaultsKeyValueStore(storeName: "test2")
+        
         let testNumber1 = 1
         let testNumber2 = 2
         
-        await keyValueStore.put(key: "1", value: testNumber1)
-        await keyValueStore.put(key: "2", value: testNumber2)
+        await keyValueStore1.put(key: "1", value: testNumber1)
+        await keyValueStore1.put(key: "2", value: testNumber2)
         
-        await keyValueStore.put(key: "1", value: testNumber1)
-        await keyValueStore.put(key: "2", value: testNumber2)
+        await keyValueStore1.put(key: "1", value: testNumber1)
+        await keyValueStore1.put(key: "2", value: testNumber2)
+        
+        await keyValueStore2.put(key: "1", value: testNumber1)
      
-        let result = await keyValueStore.listKeys()
-        let keys = AssertResultSucceded(result)
+        let result = await keyValueStore1.listKeys()
+        let keys1 = AssertResultSucceded(result)
         
-        XCTAssertEqual(keys.sorted(), ["1", "2"].sorted())
+        XCTAssertEqual(keys1.sorted(), ["1", "2"].sorted())
+        
+        let result2 = await keyValueStore2.listKeys()
+        let keys2 = AssertResultSucceded(result2)
+        
+        XCTAssertEqual(keys2.sorted(), ["1"].sorted())
     }
     
     func testDeleteKey() async {
         
+        let keyValueStore1 = UserDefaultsKeyValueStore(storeName: "test1")
+        let keyValueStore2 = UserDefaultsKeyValueStore(storeName: "test2")
+        
         let testNumber1 = 1
         let testNumber2 = 2
         
-        await keyValueStore.put(key: "1", value: testNumber1)
-        await keyValueStore.put(key: "2", value: testNumber2)
+        await keyValueStore1.put(key: "1", value: testNumber1)
+        await keyValueStore1.put(key: "2", value: testNumber2)
         
-        let resultDelete = await keyValueStore.delete(key: "1")
+        await keyValueStore2.put(key: "1", value: testNumber1)
+        
+        let resultDelete = await keyValueStore1.delete(key: "1")
         AssertResultSucceded(resultDelete)
         
-        let result = await keyValueStore.listKeys()
+        let result = await keyValueStore1.listKeys()
         let keys = AssertResultSucceded(result)
         
-        XCTAssertEqual(keys, ["1"])
+        XCTAssertEqual(keys, ["2"])
+        
+        let result2 = await keyValueStore2.listKeys()
+        let keys2 = AssertResultSucceded(result2)
+        
+        XCTAssertEqual(keys2, ["1"])
+    }
+    
+    func testDeleteAll() async {
+        
+        let keyValueStore1 = UserDefaultsKeyValueStore(storeName: "test1")
+        let keyValueStore2 = UserDefaultsKeyValueStore(storeName: "test2")
+        
+        let testNumber1 = 1
+        let testNumber2 = 2
+        
+        await keyValueStore1.put(key: "1", value: testNumber1)
+        await keyValueStore1.put(key: "2", value: testNumber2)
+        
+        await keyValueStore2.put(key: "1", value: testNumber1)
+        
+        let resultDelete = await keyValueStore1.deleteAll()
+        AssertResultSucceded(resultDelete)
+        
+        let result = await keyValueStore1.listKeys()
+        let keys = AssertResultSucceded(result)
+        
+        XCTAssertEqual(keys, [])
+        
+        let result2 = await keyValueStore2.listKeys()
+        let keys2 = AssertResultSucceded(result2)
+        
+        XCTAssertEqual(keys2, ["1"])
     }
 }
-
 
 fileprivate struct TestStruct: Codable, Equatable {
 
