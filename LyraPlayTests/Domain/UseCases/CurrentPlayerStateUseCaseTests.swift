31dcb3932c9ae1b64a6cd885abe71d0f59e48f94
Azat <azat4dev@gmail.com>
2022-07-05 01:04:22 +0300

    Move the AssertSequence into a separate file

--- LyraPlayTests/Domain/UseCases/CurrentPlayerStateUseCaseTests.swift
+++ LyraPlayTests/Domain/UseCases/CurrentPlayerStateUseCaseTests.swift
@@ -71,7 +71,6 @@ class CurrentPlayerStateUseCaseTests: XCTestCase {
         )
         
         try AssertResultSucceded(resultPlay)
-        sleep(1)
 
         
         currentTimeSequence.observe(currentPlayerStateUseCase.currentTime) { value in
@@ -225,7 +224,7 @@ fileprivate class AudioServiceMock: AudioService {
         self.fileId.value = fileId
         isPlaying.value = true
         
-        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 1) { [weak self] in
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
             
             guard
                 let self = self,
@@ -281,74 +280,5 @@ fileprivate class ShowMediaInfoUseCaseMock: ShowMediaInfoUseCase {
         }
         
         return .success(trackData)
-    }
-}
-
-class AssertSequence<T: Equatable> {
-    
-    private struct ExpectationItem {
-        var expectation: XCTestExpectation?
-        var value: T
-    }
-    
-    private unowned var testCase: XCTestCase
-    private var expectedValues: [T] = [] {
-        didSet {
-            expectation.expectedFulfillmentCount = expectedValues.count
-        }
-    }
-    
-    private var receivedValues = [T]()
-    private var expectation = XCTestExpectation()
-    
-    
-    init(testCase: XCTestCase, values: [T] = []) {
-        
-        self.testCase = testCase
-        
-        addExpectations(values: values)
-    }
-    
-    func addExpectation(value: T) {
-
-        expectedValues.append(value)
-    }
-    
-    func addExpectations(values: [T]) {
-        
-        values.forEach { value in
-            addExpectation(value: value)
-        }
-    }
-
-    func observe(_ observable: Observable<T>, file: StaticString = #filePath, line: UInt = #line) {
-        
-        observe(observable, mapper: { $0 }, file: file, line: line)
-    }
-    
-    func observe<X>(_ observable: Observable<X>, mapper: @escaping (X) -> T, file: StaticString = #filePath, line: UInt = #line) {
-        
-        
-        observable.observe(on: self) { [weak self] value in
-            
-            guard let self = self else {
-                return
-            }
-            
-            let mappedValue = mapper(value)
-            
-            self.receivedValues.append(mappedValue)
-            self.expectation.fulfill()
-        }
-    }
-    
-    func wait(timeout: TimeInterval, enforceOrder: Bool, file: StaticString = #filePath, line: UInt = #line) {
-        
-        if !enforceOrder {
-            fatalError("Not implemented")
-        }
-        
-        testCase.wait(for: [expectation], timeout: timeout, enforceOrder: enforceOrder)
-        XCTAssertEqual(receivedValues, expectedValues, file: file, line: line)
     }
 }
---
+++ LyraPlayTests/Utils/AssertSequence.swift
@@ -0,0 +1,74 @@
+//
+//  AssertSequence.swift
+//  LyraPlayTests
+//
+//  Created by Azat Kaiumov on 05.07.22.
+//
+
+import Foundation
+import XCTest
+import LyraPlay
+
+class AssertSequence<T: Equatable> {
+    
+    private unowned var testCase: XCTestCase
+    private var expectedValues: [T] = [] {
+        didSet {
+            expectation.expectedFulfillmentCount = expectedValues.count
+        }
+    }
+    
+    private var receivedValues = [T]()
+    private var expectation = XCTestExpectation()
+    
+    
+    init(testCase: XCTestCase, values: [T] = []) {
+        
+        self.testCase = testCase
+        
+        addExpectations(values: values)
+    }
+    
+    func addExpectation(value: T) {
+
+        expectedValues.append(value)
+    }
+    
+    func addExpectations(values: [T]) {
+        
+        values.forEach { value in
+            addExpectation(value: value)
+        }
+    }
+
+    func observe(_ observable: Observable<T>, file: StaticString = #filePath, line: UInt = #line) {
+        
+        observe(observable, mapper: { $0 }, file: file, line: line)
+    }
+    
+    func observe<X>(_ observable: Observable<X>, mapper: @escaping (X) -> T, file: StaticString = #filePath, line: UInt = #line) {
+        
+        
+        observable.observe(on: self) { [weak self] value in
+            
+            guard let self = self else {
+                return
+            }
+            
+            let mappedValue = mapper(value)
+            
+            self.receivedValues.append(mappedValue)
+            self.expectation.fulfill()
+        }
+    }
+    
+    func wait(timeout: TimeInterval, enforceOrder: Bool, file: StaticString = #filePath, line: UInt = #line) {
+        
+        if !enforceOrder {
+            fatalError("Not implemented")
+        }
+        
+        testCase.wait(for: [expectation], timeout: timeout, enforceOrder: enforceOrder)
+        XCTAssertEqual(receivedValues, expectedValues, file: file, line: line)
+    }
+}
