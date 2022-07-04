5c6267d0bfd8b03afbb245cdfaaafd1d1eb3c1af
Azat <azat4dev@gmail.com>
2022-07-05 01:05:41 +0300

    Move the AudioServiceMock into a separate file

--- LyraPlayTests/Domain/UseCases/CurrentPlayerStateUseCaseTests.swift
+++ LyraPlayTests/Domain/UseCases/CurrentPlayerStateUseCaseTests.swift
@@ -208,65 +208,7 @@ class CurrentPlayerStateUseCaseTests: XCTestCase {
 
 // MARK: - Mocks
 
-fileprivate class AudioServiceMock: AudioService {
-    
-    public var fileId: Observable<String?> = Observable(nil)
-    
-    public var isPlaying = Observable(false)
-    
-    public var currentTime = Observable(0.0)
-    
-    public var volume = Observable(0.0)
-
-    
-    func play(fileId: String, data: Data) async -> Result<Void, Error> {
-    
-        self.fileId.value = fileId
-        isPlaying.value = true
-        
-        DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 0.1) { [weak self] in
-            
-            guard
-                let self = self,
-                self.isPlaying.value
-            else {
-                return
-            }
-            
-            
-            self.currentTime.value += 1
-        }
-        
-        return .success(())
-    }
-    
-    func pause() async -> Result<Void, Error> {
-        
-        isPlaying.value = false
-        
-        return .success(())
-    }
-    
-    func stop() async -> Result<Void, Error> {
-        
-        fileId.value = nil
-        isPlaying.value = false
-        
-        return .success(())
-    }
-    
-    func seek(time: Double) async -> Result<Void, Error> {
-        
-        currentTime.value = time
-        return .success(())
-    }
-    
-    func setVolume(value: Double) async -> Result<Void, Error> {
-        
-        volume.value = value
-        return .success(())
-    }
-}
+fileprivate 
 
 
 fileprivate class ShowMediaInfoUseCaseMock: ShowMediaInfoUseCase {
