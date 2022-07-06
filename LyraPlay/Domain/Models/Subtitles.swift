c6f73cc5f862248c0851b310c283263e12b50305
Azat <azat4dev@gmail.com>
2022-07-06 17:16:39 +0300

    Add SubtitlesParser interface

--- LyraPlay/Domain/Models/Subtitles.swift
+++ LyraPlay/Domain/Models/Subtitles.swift
@@ -8,26 +8,20 @@
 import Foundation
 
 
-struct Subtitles {
+public struct Subtitles {
     
     public var sentences: [Sentence]
 }
 
-// MARK: - Sentence
-
 extension Subtitles {
+    
     public struct Sentence {
         
         public var startTime: Double
         public var duration: Double
         public var text: SentenceText
     }
-}
 
-// MARK: - Text
-
-extension Subtitles {
-    
     public enum SentenceText {
         
         case notSynced(text: String)
