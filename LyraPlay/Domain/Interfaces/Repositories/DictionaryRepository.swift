0ec2f73a966faf0046d22080506327bee29cf19e
Azat <azat4dev@gmail.com>
2022-07-20 13:50:19 +0300

    Move the DictionaryItem into the models folder

--- LyraPlay/Domain/Interfaces/Repositories/DictionaryRepository.swift
+++ LyraPlay/Domain/Interfaces/Repositories/DictionaryRepository.swift
@@ -9,30 +9,6 @@ import Foundation
 
 // MARK: - Interfaces
 
-public struct DictionaryItem {
-
-    public var id: UUID?
-    public var createdAt: Date?
-    public var updatedAt: Date?
-    public var originalText: String
-    public var language: String
-
-    public init(
-        id: UUID? = nil,
-        createdAt: Date? = nil,
-        updatedAt: Date? = nil,
-        originalText: String,
-        language: String
-    ) {
-        
-        self.id = id
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-        self.originalText = originalText
-        self.language = language
-    }
-}
-
 public enum DictionaryRepositoryError: Error {
     
     case itemNotFound
---
+++ LyraPlay/Domain/Models/DictionaryItem.swift
@@ -0,0 +1,32 @@
+//
+//  DictionaryItem.swift
+//  LyraPlay
+//
+//  Created by Azat Kaiumov on 20.07.22.
+//
+
+import Foundation
+
+public struct DictionaryItem {
+
+    public var id: UUID?
+    public var createdAt: Date?
+    public var updatedAt: Date?
+    public var originalText: String
+    public var language: String
+
+    public init(
+        id: UUID? = nil,
+        createdAt: Date? = nil,
+        updatedAt: Date? = nil,
+        originalText: String,
+        language: String
+    ) {
+        
+        self.id = id
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.originalText = originalText
+        self.language = language
+    }
+}
