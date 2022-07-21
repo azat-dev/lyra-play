cf709ea73990b298fb7b89cb0e5332d7505aaf82
Azat <azat4dev@gmail.com>
2022-07-21 23:34:05 +0300

    Add ProvideTranslationsForSubtitlesUseCaseTests

---
+++ LyraPlayTests/Domain/UseCases/ProvideTranslationsForSubtitlesUseCaseTests.swift
@@ -0,0 +1,177 @@
+//
+//  ProvideTranslationsForSubtitlesUseCaseTests.swift
+//  LyraPlayTests
+//
+//  Created by Azat Kaiumov on 21.07.22.
+//
+
+import Foundation
+import XCTest
+import LyraPlay
+
+class ProvideTranslationsForSubtitlesUseCaseTests: XCTestCase {
+    
+    typealias SUT = (
+        useCase: ProvideTranslationsForSubtitlesUseCase,
+        dictionaryRepository: DictionaryRepositoryMock
+    )
+    
+    func createSUT() -> SUT {
+        
+        let useCase = ProvideTranslationsForSubtitlesUseCase()
+        let dictionaryRepository = DictionaryRepositoryMock()
+        
+        detectMemoryLeak(instance: useCase)
+        
+        return (
+            useCase,
+            dictionaryRepository
+        )
+    }
+    
+    func testFetchTranslationsForEmptySubtitles() async throws {
+        
+        let sut = createSUT()
+        
+        let mediaId = UUID()
+        
+        let subtitles = Subtitles(sentences: [])
+        await sut.useCase.load(for: mediaId, subtitles: subtitles)
+        
+        let resultTranslations = await sut.useCase.fetchTranslations(words: [])
+        let items = try AssertResultSucceded(resultTranslations)
+        
+        XCTAssertTrue(items.isEmpty)
+    }
+    
+    private func anyDictionaryItem(originalText: String, translation: String, id: UUID? = nil) -> DictionaryItem {
+        
+        return .init(
+            id: id ?? UUID(),
+            createdAt: nil,
+            updatedAt: nil,
+            originalText: originalText,
+            language: "",
+            translations: [
+                TranslationItem(text: "translation")
+            ]
+        )
+    }
+    
+    func testFetchGlobalTranslations() async throws {
+        
+        let sut = createSUT()
+        let mediaId = UUID()
+        
+        let numberOfWords = 2
+        let dictionaryIds = (0..<numberOfWords).map { _ in UUID() }
+        
+        let expectedTranslations = (0..<numberOfWords).map { index in
+            
+            return ExpectedTranslation(
+                dictionaryItemId: dictionaryIds[index],
+                originalText: "original\(index)",
+                generalizedText: "generalized\(index)",
+                translation: "translation\(index)"
+            )
+        }
+        
+        sut.dictionaryRepository.items = (0..<numberOfWords).map { index in
+            
+            let id = dictionaryIds[index]
+            
+            return anyDictionaryItem(
+                originalText: "original\(index)",
+                translation: "translation\(index)",
+                id: id
+            )
+        }
+        
+        let text = expectedTranslations
+            .map { item in item.originalText }
+            .joined(separator: " ")
+        
+        let components = expectedTranslations.map { item in
+            TextComponent(type: .word, range: text.range(of: item.originalText)!)
+        }
+        
+        let subtitles = Subtitles(sentences: [
+            .init(
+                startTime: 0,
+                text: text,
+                components: components
+            )
+        ])
+        
+        await sut.useCase.prepare(for: mediaId, subtitles: subtitles)
+        
+        let resultTranslations = await sut.useCase.fetchTranslations(words: [])
+        let items = try AssertResultSucceded(resultTranslations)
+        
+        XCTAssertEqual(items.count, numberOfWords)
+        
+        for expectedTranslation in expectedTranslations {
+            
+            let item = items[expectedTranslation.originalText, default: nil]
+            
+            XCTAssertNotNil(item)
+            XCTAssertEqual(item, expectedTranslation)
+        }
+    }
+}
+
+// MARK: - Mocks
+
+final class DictionaryRepositoryMock: DictionaryRepository {
+    
+    var items = [DictionaryItem]()
+    
+    func putItem(_ item: DictionaryItem) async -> Result<DictionaryItem, DictionaryRepositoryError> {
+        
+        let index = items.firstIndex { $0.id == item.id }
+        
+        guard let index = index else {
+            
+            items.append(item)
+            return .success(item)
+        }
+        
+        items[index] = item
+        return .success(item)
+    }
+    
+    func getItem(id: UUID) async -> Result<DictionaryItem, DictionaryRepositoryError> {
+        
+        let item = items.first { $0.id == id }
+        
+        guard let item = item else {
+            
+            return .failure(.itemNotFound)
+        }
+        
+        return .success(item)
+    }
+    
+    func deleteItem(id: UUID) async -> Result<Void, DictionaryRepositoryError> {
+        
+        let index = items.firstIndex { $0.id == id }
+        
+        guard let index = index else {
+            
+            return .failure(.itemNotFound)
+        }
+
+        items.remove(at: index)
+        return .success(())
+    }
+}
+
+// MARK: - Helpers
+
+struct ExpectedTranslation {
+    
+    var dictionaryItemId: UUID
+    var originalText: String
+    var generalizedText: String
+    var translation: String
+}
