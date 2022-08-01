960b1a5ea8adfd83301c5e0bbd9375636b35cb06
Azat <azat4dev@gmail.com>
2022-08-01 11:14:05 +0300

    Add SubtitlesIteratorFactory

---
+++ LyraPlay/Data/Utils/Timeline/SubtitlesIteratorFactory.swift
@@ -0,0 +1,25 @@
+//
+//  SubtitlesIteratorFactory.swift
+//  LyraPlay
+//
+//  Created by Azat Kaiumov on 01.08.22.
+//
+
+import Foundation
+
+// MARK: - Interfaces
+
+public protocol SubtitlesIteratorFactory {
+    
+    func create(for subtitles: Subtitles) -> SubtitlesIterator
+}
+
+// MARK: - Implementations
+
+public final class DefaultSubtitlesIteratorFactory: SubtitlesIteratorFactory {
+    
+    public func create(for subtitles: Subtitles) -> SubtitlesIterator {
+        
+        return DefaultSubtitlesIterator(subtitles: subtitles)
+    }
+}
