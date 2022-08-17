3059c212676b6e5d29ff1a85bbd9215f21ac07de
Azat <azat4dev@gmail.com>
2022-08-17 11:08:18 +0300

    Decouple AudioSession

---
+++ LyraPlayTests/Data/Services/AudioSessionTests.swift
@@ -0,0 +1,41 @@
+//
+//  AudioSessionTests.swift
+//  LyraPlayTests
+//
+//  Created by Azat Kaiumov on 17.08.2022.
+//
+
+import XCTest
+import AVFoundation
+
+import LyraPlay
+
+ class AudioSessionTests: XCTestCase {
+
+    typealias SUT = AudioSession
+
+    func createSUT() -> SUT {
+
+        let useCase = DefaultAudioSession()
+        detectMemoryLeak(instance: useCase)
+
+        return useCase
+    }
+
+    func test_activate() async throws {
+
+        let sut = createSUT()
+        
+        let result = sut.activate()
+        try AssertResultSucceded(result)
+    }
+
+    func test_deactivate() async throws {
+
+        let sut = createSUT()
+        
+        sut.activate()
+        let result = sut.deactivate()
+        try AssertResultSucceded(result)
+    }
+}
