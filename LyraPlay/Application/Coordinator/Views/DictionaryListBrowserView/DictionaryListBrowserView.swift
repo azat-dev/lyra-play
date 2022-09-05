6f23dfae454c658fa9fdbff4a091def3fe60319e
Azat <azat4dev@gmail.com>
2022-09-05 12:34:06 +0300

    Add protocol for the DictionaryListBrowserView

---
+++ LyraPlay/Application/Coordinator/Views/DictionaryListBrowserView/DictionaryListBrowserView.swift
@@ -0,0 +1,11 @@
+//
+//  DictionaryListBrowserView.swift
+//  LyraPlay
+//
+//  Created by Azat Kaiumov on 05.09.2022.
+//
+
+public protocol DictionaryListBrowserView: PresentableView {
+
+    init(viewModel: DictionaryListBrowserViewModel)
+}
