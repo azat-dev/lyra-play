90a03b7fe950cc516e7f4c00360d948b932cf894
Azat <azat4dev@gmail.com>
2023-01-19 03:18:19 +0300

    Add the ProvideFileUrlUseCaseImplFactory

--- LyraPlay/Domain/UseCases/ProvideFileUrlUseCase/ProvideFileUrlUseCaseFactory.swift
+++ LyraPlay/Domain/UseCases/ProvideFileUrlUseCase/ProvideFileUrlUseCaseFactory.swift
@@ -5,7 +5,11 @@
 //  Created by Azat Kaiumov on 18.01.2023.
 //
 
+import Foundation
+
+public typealias ProvideFileUrlUseCaseCallback = () -> URL
+
 public protocol ProvideFileUrlUseCaseFactory {
 
-    func create() -> ProvideFileUrlUseCase
+    func create(callback: @escaping ProvideFileUrlUseCaseCallback) -> ProvideFileUrlUseCase
 }
