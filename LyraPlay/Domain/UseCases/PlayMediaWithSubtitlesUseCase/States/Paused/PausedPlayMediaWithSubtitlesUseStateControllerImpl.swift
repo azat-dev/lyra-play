ae01883adbde3eb292fc5ec2de2946295f601acd
Azat <azat4dev@gmail.com>
2023-02-20 00:08:30 +0300

    Add an implementation of the PausedPlayMediaWithSubtitlesUseStateController

--- LyraPlay/Domain/UseCases/PlayMediaWithSubtitlesUseCase/States/Paused/PausedPlayMediaWithSubtitlesUseStateControllerImpl.swift
+++ LyraPlay/Domain/UseCases/PlayMediaWithSubtitlesUseCase/States/Paused/PausedPlayMediaWithSubtitlesUseStateControllerImpl.swift
@@ -10,8 +10,36 @@ import Foundation
 public class PausedPlayMediaWithSubtitlesUseStateControllerImpl: PlayingPlayMediaWithSubtitlesUseStateControllerImpl, PausedPlayMediaWithSubtitlesUseStateController {
     
     // MARK: - Methods
+
+    public override func play() -> Result<Void, PlayMediaWithSubtitlesUseCaseError> {
+    
+        guard let delegate = delegate else {
+            return .failure(.internalError(nil))
+        }
+        
+        return delegate.play(session: session)
+    }
+    
+    public override func togglePlay() -> Result<Void, PlayMediaWithSubtitlesUseCaseError> {
+        
+        return play()
+    }
     
     public override func run() -> Result<Void, PlayMediaWithSubtitlesUseCaseError> {
-        return .failure(.internalError(nil))
+        
+        guard let delegate = delegate else {
+            return .failure(.internalError(nil))
+        }
+        
+        let result = session.playMediaUseCase.pause()
+        
+        guard case .success = result else {
+            return result.mapResult()
+        }
+        
+        session.playSubtitlesUseCase?.pause()
+        delegate.didPause(session: session)
+        
+        return .success(())
     }
 }
