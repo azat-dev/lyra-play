666721e353598d42b2cccbc8683a2d5c0a922ec8
Azat <azat4dev@gmail.com>
2023-04-06 07:29:09 +0300

    Refactor LoadedAudioPlayerStateControllerTests

--- LyraPlayTests/Data/Services/AudioPlayerTests/LoadedAudioPlayerStateControllerTests.swift
+++ LyraPlayTests/Data/Services/AudioPlayerTests/LoadedAudioPlayerStateControllerTests.swift
@@ -24,7 +24,6 @@ class LoadedAudioPlayerStateControllerTests: XCTestCase {
         let delegate = mock(AudioPlayerStateControllerDelegate.self)
         
         let fileId = UUID().uuidString
-        let fileData = try! getTestFile()
         
         let session = ActiveAudioPlayerStateControllerSession(
             fileId: fileId,
@@ -56,7 +55,7 @@ class LoadedAudioPlayerStateControllerTests: XCTestCase {
         
         let newFileId = "new_file_id"
         let newFileData = "new_file_data".data(using: .utf8)!
-
+        
         given(sut.delegate.load(fileId: any(), data: any()))
             .willReturn(.success(()))
         
@@ -116,7 +115,7 @@ class LoadedAudioPlayerStateControllerTests: XCTestCase {
         // Then
         verify(
             sut.delegate.startPlaying(
-                atTime: 0, 
+                atTime: 0,
                 session: any(
                     ActiveAudioPlayerStateControllerSession.self,
                     where: { $0.fileId == session.fileId }
@@ -176,15 +175,5 @@ class LoadedAudioPlayerStateControllerTests: XCTestCase {
                 )
             )
         ).wasCalled(1)
-    }
-    
-    // MARK: - Helpers
-    
-    private func getTestFile(name: String = "test_music_with_tags") throws -> Data {
-        
-        let bundle = Bundle(for: type(of: self ))
-        let url = bundle.url(forResource: name, withExtension: "mp3")!
-        
-        return try Data(contentsOf: url)
     }
 }
